110b0544f0d3f50a06a17d7e79cc9dc4c968774c
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-11-14 19:56:51 -0300

    feat: basic nodes setup background player and score label

--- SpaceRace/GameScene.swift
+++ SpaceRace/GameScene.swift
@@ -7,9 +7,45 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        // setup background with stars particles
+        backgroundColor = .black
+        
+        starfield = SKEmitterNode(fileNamed: "starfield")!
+        starfield.position = CGPoint(x: 1024, y: 384)
+        // populate 10 seconds of particles before starting
+        starfield.advanceSimulationTime(10)
+        addChild(starfield)
+        starfield.zPosition = -1
+        
+        // setup player
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        // enable collisions with the scene objects
+        player.physicsBody?.contactTestBitMask = 1
+        player.physicsBody?.affectedByGravity = false
+        addChild(player)
+        
+        // setup score label
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        
+        score = 0
     }
     
     override func update(_ currentTime: TimeInterval) {
