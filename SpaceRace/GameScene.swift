a227b7877d4e0f95a7172ee41b516ab311acc9e3
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-11-14 19:29:16 -0300

    Initial commit

---
+++ SpaceRace/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  SpaceRace
+//
+//  Created by Rodrigo on 14-11-24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
